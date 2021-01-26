5ed2d010b9525de0b0e5f81b799aa6dc288cd98a
Jani Mäkelä <jmakela@synack.com>
2021-01-26 23:30:31 +0200

    Correctly highlight dropdown options

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -1,4 +1,4 @@
-import { useHistory } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
@@ -6,6 +6,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown'
 
 function AppNavbar() {
   let history = useHistory()
+  let location = useLocation()
 
   const handleNavigate = (url) => {
     return () => {
@@ -20,11 +21,31 @@ function AppNavbar() {
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav>
           <NavDropdown title="Problems">
-            <NavDropdown.Item onClick={handleNavigate('/problems')}>All problems</NavDropdown.Item>
+            <NavDropdown.Item
+              active={location.pathname === '/problems'}
+              onClick={handleNavigate('/problems')}
+            >
+              All problems
+            </NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item onClick={handleNavigate('/problems/employee-count')}>Employee count problem</NavDropdown.Item>
-            <NavDropdown.Item onClick={handleNavigate('/problems/worker-count')}>Worker count problem</NavDropdown.Item>
-            <NavDropdown.Item onClick={handleNavigate('/problems/fixed-costs')}>Fixed costs problem</NavDropdown.Item>
+            <NavDropdown.Item
+              active={location.pathname === '/problems/employee-count'}
+              onClick={handleNavigate('/problems/employee-count')}
+            >
+              Employee count problem
+            </NavDropdown.Item>
+            <NavDropdown.Item
+              active={location.pathname === '/problems/worker-count'}
+              onClick={handleNavigate('/problems/worker-count')}
+            >
+              Worker count problem
+            </NavDropdown.Item>
+            <NavDropdown.Item
+              active={location.pathname === '/problems/fixed-costs'}
+              onClick={handleNavigate('/problems/fixed-costs')}
+            >
+              Fixed costs problem
+            </NavDropdown.Item>
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
