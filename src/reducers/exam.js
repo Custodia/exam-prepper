fa3a60f560b37ea5d507025d8cd1d78411d3c64b
Jani Mäkelä <jmakela@synack.com>
2021-01-30 11:45:45 +0200

    Persist exam answers in redux store

--- src/reducers/exam.js
+++ src/reducers/exam.js
@@ -1,13 +1,24 @@
 export const INITIAL_STATE = {
-  seed: Math.random()
+  seed: Math.random(),
+  answers: {}
 }
 
 export const SET_EXAM_SEED = 'SET_EXAM_SEED'
+export const SET_EXAM_ANSWERS = 'SET_EXAM_ANSWERS'
 
 export default function(state = INITIAL_STATE, action) {
   switch (action.type) {
     case SET_EXAM_SEED:
       return { ...state, seed: action.seed }
+
+    case SET_EXAM_ANSWERS:
+      return {
+        ...state,
+        answers: {
+          ...state.answers,
+          ...action.answers
+        }
+      }
 
     default:
       return state
