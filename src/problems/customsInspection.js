75cc4ef005f227540e7ddb175e6b8e930520cd9a
Jani Mäkelä <jmakela@synack.com>
2021-02-28 16:06:13 +0200

    Fix RNG in customs inspection problem

--- src/problems/customsInspection.js
+++ src/problems/customsInspection.js
@@ -2,7 +2,7 @@ export const id = 'customs-inspection'
 export const problemTitle = 'Customs inspection problem'
 
 export const getProblem = (rng) => {
-  const customsChance = [5, 10][Math.floor(Math.random() * 2)] / 100
+  const customsChance = [5, 10][Math.floor(rng() * 2)] / 100
   const correctAnswer = (1 - customsChance) * (1 - customsChance)
 
   const problemStatement = `The probability that a passenger is chosen for customs inspection is ${customsChance * 100} %. Emma and Michael go through customs. What is the probability that neither of them is chosen for inspection?`
