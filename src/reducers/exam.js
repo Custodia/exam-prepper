8e3261a0a47cf3a9924f122170770f233a3f14fa
Jani Mäkelä <jmakela@synack.com>
2021-02-28 17:17:00 +0200

    Fix eslint issues

--- src/reducers/exam.js
+++ src/reducers/exam.js
@@ -10,30 +10,32 @@ export const INITIALIZE_EXAM = 'INITIALIZE_EXAM'
 export const SET_EXAM_STATE = 'SET_EXAM_STATE'
 export const SUBMIT_EXAM = 'SUBMIT_EXAM'
 
-export default function(state = INITIAL_STATE, action) {
+const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case INITIALIZE_EXAM:
-      return {
-        ...state,
-        ...INITIAL_STATE,
-        seed: action.seed || Math.random(),
-        startTime: Date.now()
-      }
+  case INITIALIZE_EXAM:
+    return {
+      ...state,
+      ...INITIAL_STATE,
+      seed: action.seed || Math.random(),
+      startTime: Date.now()
+    }
 
-    case SET_EXAM_STATE:
-      return {
-        ...state,
-        answers: action.answers,
-        touched: action.touched
-      }
+  case SET_EXAM_STATE:
+    return {
+      ...state,
+      answers: action.answers,
+      touched: action.touched
+    }
 
-    case SUBMIT_EXAM:
-      return {
-        ...state,
-        endTime: action.endTime || Date.now()
-      }
+  case SUBMIT_EXAM:
+    return {
+      ...state,
+      endTime: action.endTime || Date.now()
+    }
 
-    default:
-      return state
+  default:
+    return state
   }
 }
+
+export default reducer
