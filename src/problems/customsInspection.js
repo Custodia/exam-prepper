d8a9794811723298006c2b5a98b8fe8ad8b72ec2
Jani Mäkelä <jmakela@synack.com>
2021-02-28 21:36:48 +0200

    Update customs chance problem to use bignumber

--- src/problems/customsInspection.js
+++ src/problems/customsInspection.js
@@ -1,12 +1,18 @@
+import BigNumber from "bignumber.js"
+import { selectRandomArrayElement } from '../helpers'
+
 export const id = 'customs-inspection'
 export const problemTitle = 'Customs inspection problem'
 
 export const getProblem = (rng) => {
-  const customsChance = [5, 10][Math.floor(rng() * 2)] / 100
-  const correctAnswer = (1 - customsChance) * (1 - customsChance)
+  const customsChanceString = selectRandomArrayElement(rng)([6, 7, 8, 9, 11, 12])
+  const customsChance = new BigNumber(customsChanceString).dividedBy(100)
+  const inverseCustomsChance = (new BigNumber(1)).minus(customsChance)
+  const correctAnswer = inverseCustomsChance.times(inverseCustomsChance)
 
-  const problemStatement = `The probability that a passenger is chosen for customs inspection is ${customsChance * 100} %. Emma and Michael go through customs. What is the probability that neither of them is chosen for inspection?`
-  const isAnswerCorrect = (answer) => [`${correctAnswer}`, `${correctAnswer * 100}`, `${correctAnswer * 100}%`].includes(answer)
+  const problemStatement = `The probability that a passenger is chosen for customs inspection is ${customsChanceString} %. Emma and Michael go through customs. What is the probability that neither of them is chosen for inspection?`
+  const isAnswerCorrect = (answer) =>
+        [correctAnswer.toString(), correctAnswer.times(100).toString(), `${correctAnswer.times(100).toString()}%`].includes(answer)
 
   return {
     problemStatement,
