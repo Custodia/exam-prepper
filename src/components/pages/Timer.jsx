fc769c94167bd455526b52d04bdf4971b7eb8ea1
Jani Mäkelä <jmakela@synack.com>
2021-02-28 16:21:56 +0200

    Make sure timer numbers never show up as negative

--- src/components/pages/Timer.jsx
+++ src/components/pages/Timer.jsx
@@ -16,11 +16,11 @@ export class Timer extends PureComponent {
   render() {
     const comparisonTime = this.props.endTime || this.state.currentTime
     const duration = comparisonTime - this.props.startTime
-    const seconds = Math.floor(duration / 1000) % 60
+    const seconds = Math.max(Math.floor(duration / 1000) % 60, 0)
     const secondsString = seconds.toString().padStart(2, '0')
-    const minutes = Math.floor(duration / (60 * 1000)) % 60
+    const minutes = Math.max(Math.floor(duration / (60 * 1000)) % 60, 0)
     const minutesString = minutes.toString().padStart(2, '0')
-    const hours = Math.floor(duration / (60 * 60 * 1000))
+    const hours = Math.max(Math.floor(duration / (60 * 60 * 1000)), 0)
     const hoursString = hours != 0 ? `${hours}:` : ''
 
     return (
