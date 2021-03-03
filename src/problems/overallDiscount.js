c0795fc58ffee9faa8061468318f1b0ad823839d
Jani Mäkelä <jmakela@synack.com>
2021-03-03 23:07:39 +0200

    Fix over all discount correct answer calculation

--- src/problems/overallDiscount.js
+++ src/problems/overallDiscount.js
@@ -7,7 +7,7 @@ const problemTitle = 'Overall discount problem'
 const getProblem = (rng) => {
   const discount1 = new BigNumber(randomIntegerBetween(rng)(60, 80)).dividedBy(100)
   const discount2 = new BigNumber(randomIntegerBetween(rng)(60, 80)).dividedBy(100)
-  const overAllDiscount = (new BigNumber(1)).minus(discount1.times(discount2))
+  const overAllDiscount = discount1.times(discount2)
 
   const discount1String = 100 - discount1.times(100).toFixed()
   const discount2String = 100 - discount2.times(100).toFixed()
